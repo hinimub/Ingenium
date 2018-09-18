df28f51a65a38ee01f0ee3c9cf9a3fe9b999ed86
hinimub <takumab21@gmail.com>
2018-09-19 00:16:26 +0530

    Delete interface.js

--- src/interface.js
+++
@@ -1,8 +0,0 @@
-function submit(e) {
-  var mode = e.parameters.mode;
-  
-  if(Object.keys(e.formInput).length) var inputs = e.formInput;
-  else var inputs = JSON.parse(e.parameters.inputs);
-  
-  return dispatcher(mode, inputs);
-}
